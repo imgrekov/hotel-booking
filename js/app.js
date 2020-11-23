c6237c77a66bf211e60c119102c2994cb170d34f
Gregory Grekov <d9fgrek@gmail.com>
2020-11-24 00:03:36 +0300

    Fix: add function keyboard slide

--- js/app.js
+++ js/app.js
@@ -1,10 +1,13 @@
 const hotelSlider = new Swiper('.hotel-slider', {
+  spaceBetween: 10,
   loop: true,
+
+  keyboard: {
+    enabled: true,
+  },
 
   navigation: {
     nextEl: '.hotel-slider__nav--next',
     prevEl: '.hotel-slider__nav--prev',
   },
-
-  spaceBetween: 10,
 })
