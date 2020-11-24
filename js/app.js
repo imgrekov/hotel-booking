6e7e15d74f6948fed4fc9392b01c0a8aa796b6ab
Gregory Grekov <d9fgrek@gmail.com>
2020-11-24 18:28:48 +0300

    Create: block hotel options & hotel map

--- js/app.js
+++ js/app.js
@@ -11,3 +11,14 @@ const hotelSlider = new Swiper('.hotel-slider', {
     prevEl: '.hotel-slider__nav--prev',
   },
 })
+
+// Функция ymaps.ready() будет вызвана, когда
+// загрузятся все компоненты API, а также когда будет готово DOM-дерево.
+ymaps.ready(init)
+function init() {
+  const hotelMap = new ymaps.Map('hotel-map', {
+    center: [-8.825708188621496, 115.21862255745404],
+    zoom: 16,
+  })
+  hotelMap.setType('yandex#hybrid')
+}
